2a622a64d29f4dd11acdcecc8b7e2f5e2cd5533e
Saumya <saumya.jajodia@gmail.com>
2023-04-01 16:29:59 +0530

    intro to jsx commit

--- App.js
+++ App.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
+const heading = React.createElement("h1", 
+{
+    id:"heading"
+}, "Namastee React using ReactcreateElement");
 
-// Manipulate the HTML DOM using React
+console.log(heading);
 
-// Create nested React Elements
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading1"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading1]
-  );
-  
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(container);
+const jsxHeading = <h1 id='jsxheading'>Namaste React using JSX</h1>;
+
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
