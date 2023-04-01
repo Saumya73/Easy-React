79da32a748b887f23ff8d73a2df7cb021785a23f
Saumya <saumya.jajodia@gmail.com>
2023-04-01 18:12:14 +0530

    component composition

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 
-// const heading = React.createElement("h1", 
-// {
-//     id:"heading"
-// }, "Namastee React using ReactcreateElement");
+const heading = React.createElement("h1", 
+{
+    id:"heading"
+}, "Namastee React using React Create Element");
 
 // console.log(heading);
 
@@ -17,13 +17,25 @@ const jsxHeading = <h1 id='jsxheading'>Namaste React using JSX</h1>;    //react 
 //React Functional Component
 
 const HeadingComponent1 = () => {
-    return <h2>Namste Functional Component</h2>;
+    return <h2>Namste Functional Component 1</h2>;
 };
 
+const number = 100;
+
+//Component composition - passing component inside component
+
 const HeadingComponent2 = () => (
-     <h2>Namste Functional Component</h2>                               //both function component are same
+    <div>
+    <HeadingComponent1/>                                                   
+    {heading}
+    <h2>{number}</h2>
+    {number}
+  <h4>  {2+5+1+1+9+9+8} </h4>
+  <h5>{console.log("ggddd")}</h5>
+     <h2>Namste Functional Component 2</h2>    
+     </div>                          
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent1/>);
+root.render(<HeadingComponent2/>);
